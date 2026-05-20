ab650a0e9b278ec58df9843c037125e96ef2df59
gjinbeka <gjinbeka11@gmail.com>
2026-05-20 19:02:14 +0200

    module29

---
+++ module29/main.js
@@ -0,0 +1,37 @@
+var programming=document = ['java','html','python'];
+
+console.log(programmingL);
+
+console.log(programmingL[1]);
+
+programmingL.push('css');
+console.log(programmingL);
+
+programmingL.pop();
+console.log(programmingL);
+
+programmingL.unshift('react');
+console.log(programmingL);
+
+programmingL.shift();
+console.log(programmingL);
+
+programmingL.splice(1,1,'javascript');
+console.log(programmingL);
+
+console.log(Math.floor(Math.random()*100));
+
+
+var students = ['darisi','donarti'];
+
+var [s1,s2] = students;
+
+console.log(s1);
+
+var places = ['paris','london','new york'];
+
+var [,firstplace,,secondplace] = places;
+
+console.log(firstplace);
+
+
